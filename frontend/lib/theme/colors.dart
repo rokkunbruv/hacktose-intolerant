b85e1aa09d4980bb15796dc5ac45794ea7a23c4d
rokkunbruv <themorghiknight579@gmail.com>
2025-02-24 23:27:25 +0800

    added app color and text styles

---
+++ frontend/lib/theme/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color bg = Color(0xFFFEFDFB);
+  static final Color red = Color(0xFFAE2628);
+  static final Color vanilla = Color(0xFFFDF0CE);
+  static final Color saffron = Color(0xFFFFC75E);
+  static final Color yellow = Color(0xFFFFA600);
+  static final Color navy = Color(0xFF1D436E);
+  static final Color lightNavy = Color(0xFF95AAC2);
+  static final Color skyBlue = Color(0xFFE9F3FF);
+  static final Color gray = Color(0xFFBFBFBF);
+  static final Color lightGray = Color(0xFFF3F3F3);
+  static final Color white = Color(0xFFFFFFFF);
+  static final Color black =  Color(0xFF000000);
+}
