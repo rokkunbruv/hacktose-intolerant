e992876bc48a9ebbb84b48ffdd1bd741c3826be5
rang <aeravitorillo08@gmail.com>
2025-02-28 12:39:59 +0800

    fixed draggable container

--- frontend/lib/pages/route/route_steps_page.dart
+++ frontend/lib/pages/route/route_steps_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 // import 'package:tultul/theme/text_styles.dart';
 
 import 'package:tultul/constants/step_types.dart';
+import 'package:tultul/widgets/generic/draggable_container.dart';
 
 import 'package:tultul/widgets/steps/step_details.dart';
 
@@ -19,22 +20,24 @@ class _RouteStepsPageState extends State<RouteStepsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(40),
-        child: Column(
-          children: [
-            StepDetails(type: StepType.start),
-            StepDetails(
-                type: StepType.walk, duration: '1 min', distance: '3 km'),
-            StepDetails(
-              type: StepType.transport,
-              type2: StepType.end,
-              location: 'Insular Square',
-              jeepCode: '24',
-              fare: 'P 18.00',
-              dropOff: 'Andoks, Mabolo, Cebu City',
-            ),
-          ],
+      body: DraggableContainer(
+        child: Padding(
+          padding: EdgeInsets.all(40),
+          child: Column(
+            children: [
+              StepDetails(type: StepType.start),
+              StepDetails(
+                  type: StepType.walk, duration: '1 min', distance: '3 km'),
+              StepDetails(
+                type: StepType.transport,
+                type2: StepType.end,
+                location: 'Insular Square',
+                jeepCode: '24',
+                fare: 'P 18.00',
+                dropOff: 'Andoks, Mabolo, Cebu City',
+              ),
+            ],
+          ),
         ),
       ),
     );
